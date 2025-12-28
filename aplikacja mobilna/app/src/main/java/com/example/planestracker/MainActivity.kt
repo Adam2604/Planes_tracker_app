b1b6ce04787ad7d697085740dd0b9fecba5bd646
Adam2604 <adammichala11@gmail.com>
2025-12-28 14:45:45 +0100

    Aplikacja mobilna cd

--- aplikacja mobilna/app/src/main/java/com/example/planestracker/MainActivity.kt
+++ aplikacja mobilna/app/src/main/java/com/example/planestracker/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.planestracker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Znajdź element WebView
+        val myWebView: WebView = findViewById(R.id.webview)
+
+        // Włącz JavaScript (wymagany przez mapy Leaflet!)
+        myWebView.settings.javaScriptEnabled = true
+
+        // Wyłącz zoomowanie systemowe (mapa ma swój własny zoom)
+        myWebView.settings.builtInZoomControls = false
+
+        // Spraw, by linki otwierały się w aplikacji, a nie w Chrome
+        myWebView.webViewClient = WebViewClient()
+
+        // ZAŁADUJ STRONĘ Z MALINKI
+        // Zmień ten adres na IP swojej Malinki!
+        myWebView.loadUrl("http://192.168.0.50:5000")
     }
 }
